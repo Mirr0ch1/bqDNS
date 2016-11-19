6acd1ab641e24d16d25a23612a7ab09def9e82fe
Mirr0ch1 <mirro@live.cn>
2016-11-19 15:29:15 +0800

    Update update_dns_records.sh

--- update_dns_records.sh
+++ update_dns_records.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/sh
 
 cd /etc/dnsmasq.d
-rm -f accchina.conf
 rm -f modified.conf
-wget https://raw.githubusercontent.com/Mirr0ch1/bqDNS/master/bin/accchina.conf
 wget https://raw.githubusercontent.com/Mirr0ch1/bqDNS/master/bin/modified.conf
 cd
 service dnsmasq restart
