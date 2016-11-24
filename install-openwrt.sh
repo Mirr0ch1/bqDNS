f37991811519cf7c184112c8aeee4da47830921d
Mirr0ch1 <mirro@live.cn>
2016-11-25 00:00:57 +0800

    Update install-openwrt.sh

--- install-openwrt.sh
+++ install-openwrt.sh
@@ -28,3 +28,5 @@ echo '|      The script was finish.Please Check!         |'
 echo '| bqDNS Project :https://github.com/Mirr0ch1/bqDNS |'
 echo '|      Thanks to molin1987 s PandaDNS Project      |'
 echo '|-------------------ENJOY IT!----------------------|'
+
+rm -f install-openwrt.sh
