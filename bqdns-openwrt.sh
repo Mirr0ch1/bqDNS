f9fe31c095d13017c2db2606e46bc67657295e62
Mirr0ch1 <mirro@live.cn>
2016-11-25 15:59:28 +0800

    Update bqdns-openwrt.sh

--- bqdns-openwrt.sh
+++ bqdns-openwrt.sh
@@ -13,8 +13,8 @@ echo "server=8.8.8.8" >> /etc/dnsmasq.conf
 echo "server=8.8.4.4" >> /etc/dnsmasq.conf
 echo "conf-dir=/etc/dnsmasq.d" >> /etc/dnsmasq.conf
 cd /etc/dnsmasq.d
-wget https://raw.githubusercontent.com/Mirr0ch1/bqDNS/master/bin/accchina.conf
-wget https://raw.githubusercontent.com/Mirr0ch1/bqDNS/master/bin/modified.conf
+wget --no-check-certificate https://raw.githubusercontent.com/Mirr0ch1/bqDNS/master/bin/accchina.conf
+wget --no-check-certificate https://raw.githubusercontent.com/Mirr0ch1/bqDNS/master/bin/modified.conf
 cd
 
 echo '|--------------------------------------------------|'
