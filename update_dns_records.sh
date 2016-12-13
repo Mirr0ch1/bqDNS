0aef338db7a19b244fbf8dc4cff15723845d959e
Mirr0ch1 <mirro@live.cn>
2016-12-13 20:03:18 +0800

    Update update_dns_records.sh

--- update_dns_records.sh
+++ update_dns_records.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/sh
 
+mv /etc/hosts /etc/hosts.bak
+echo "218.254.1.15  raw.githubusercontent.com" > /etc/hosts
 cd /etc/dnsmasq.d
 rm -f modified.conf
-wget https://raw.githubusercontent.com/Mirr0ch1/bqDNS/master/bin/modified.conf
+curl -O -k https://raw.githubusercontent.com/Mirr0ch1/bqDNS/master/bin/modified.conf
 cd
 service dnsmasq restart
 
 echo "|----Process complete----|"
-
+rm -f /etc/hosts
+mv /etc/hosts.bak /etc/hosts
 rm -f update_dns_records.sh
