32cb26c58bad5a9a55966435e7c5523d75f50587
Mirr0ch1 <mirro@live.cn>
2016-12-13 18:44:44 +0800

    Create bqdns-ubuntu.sh

---
+++ bqdns-ubuntu.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+echo '|-------------------Installing---------------------|'
+echo '|            dnsmasq openssl bind-utils            |'
+echo '|--------------------------------------------------|'
+
+apt-get update
+apt-get remove iptables -y
+apt-get upgrade -y
+apt-get install dnsmasq -y
+service dnsmasq stop
+
+echo '|-------------------Configure----------------------|'
+echo '|..........rewrite the configuration file..........|'
+echo '|--------------------------------------------------|'
+
+
+rm -f /etc/dnsmasq.conf
+touch /etc/dnsmasq.conf
+echo "no-resolv" > /etc/dnsmasq.conf
+echo "no-poll" >> /etc/dnsmasq.conf
+echo "server=8.8.8.8" >> /etc/dnsmasq.conf
+echo "server=208.67.222.220" >> /etc/dnsmasq.conf
+echo "no-dhcp-interface=eth0" >> /etc/dnsmasq.conf
+echo "no-dhcp-interface=eth1" >> /etc/dnsmasq.conf
+echo "conf-dir=/etc/dnsmasq.d" >> /etc/dnsmasq.conf
+cd /etc/dnsmasq.d
+wget https://raw.githubusercontent.com/Mirr0ch1/bqDNS/master/bin/accchina.conf
+wget https://raw.githubusercontent.com/Mirr0ch1/bqDNS/master/bin/modified.conf
+cd
+
+
+echo '|-----------------Final treatment------------------|'
+echo '|--------Set SELinux and turn firewall off---------|'
+echo '|--------------------------------------------------|'
+setenforce 0
+sed -i 's/SELINUX=enforcing/SELINUX=disabled/' /etc/selinux/config
+service dnsmasq start
+
+echo '|-------------------COMPLETE-----------------------|'
+echo '|      The script was finish.Please Check!         |'
+echo '| bqDNS Project :https://github.com/Mirr0ch1/bqDNS |'
+echo '|      Thanks to molin1987 s PandaDNS Project      |'
+echo '|-------------------ENJOY IT!----------------------|'
+
+rm -f bqdns-ubuntu.sh
